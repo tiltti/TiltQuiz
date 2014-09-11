946ec189c590484a90d136781420e0acb078024f
Ossi Tiltti <ossi.tiltti@gmail.com>
2014-09-11 19:35:05 +0300

    Controller and xib

---
+++ TILTQuizViewController.h
@@ -0,0 +1,13 @@
+//
+//  TILTQuizViewController.h
+//  TiltQuiz
+//
+//  Created by Ossi Tiltti on 11/09/14.
+//  Copyright (c) 2014 Tiltti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TILTQuizViewController : UIViewController
+
+@end
